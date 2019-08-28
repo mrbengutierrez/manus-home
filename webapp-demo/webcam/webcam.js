4b274482cb9d2021ee879a850f9253b86e4c9876
mrbengutierrez <mrbengutierrez@gmail.com>
2019-08-28 12:32:05 -0400

    webcam overlay basic functionality

--- webapp-demo/webcam/webcam.js
+++ webapp-demo/webcam/webcam.js
@@ -1,5 +1,9 @@
 var video = document.querySelector("#videoElement");
 
+//var overlay = documetn.getElementbyID("videoElement");
+//overlay.style.visibility == "visible";
+//window.scrollTo(0,0);
+
 if (navigator.mediaDevices.getUserMedia) {
   navigator.mediaDevices.getUserMedia({ video: true })
     .then(function (stream) {
